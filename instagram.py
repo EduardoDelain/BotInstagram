33266e6e58343508efa461b0030f7365fb3d025d
Eduardo <epdelain@artit.com.br>
2022-03-29 21:35:30 -0300

    codigo da aplicacao

--- instagram.py
+++ instagram.py
@@ -1,58 +1,74 @@
 import tkinter as tk
 from tkinter import ttk
-import selenium
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-import datetime
+import time
 
-lista_tipos = ['Sem Parar','Veloe']
-lista_codigos = []
+def seguidores():
+    usuario = user.get()
+    senha = password.get()
+    pessoa = link.get()
+    numero = int(num.get())
 
-def codigo():
-    login = entry_login.get()
-    tipo = combobox_meio.get()
-    data = datetime.datetime.now()
-    data_criacao = data.strftime("%H/%M/%S")
-    codigos = len(lista_codigos)+1
-    codigo_str = "COD-{}".format(codigos)
-    lista_codigos.append((codigo_str, login, tipo, data_criacao))
+    # Entrando no Instagram
+    navegador = webdriver.Chrome()
+    navegador.get('https://www.instagram.com/')
+    time.sleep(3)
 
-    texto_login["text"] = lista_codigos
+    # Fazendo login no instagram
+    navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(usuario)
+    time.sleep(2)
+    navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(senha)
+    time.sleep(2)
+    navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]').click()
 
-def abrir_janela():
-    janela2 = tk.Toplevel()
-    janela2.title('Janela nova')
-    label_nome = tk.Label(janela2, text="nome")
-    label_nome.grid(row=0, column=0, padx=10, pady=10, sticky='nswe', columnspan=1)
-    botao3 = tk.Button(janela2, text="Fechar janela", command=janela2.destroy)
-    botao3.grid(row=1, column=0, padx=10, pady=10, sticky='nswe', columnspan=1)
+    # acessando os seguidores da outra pessoa
+    time.sleep(3)
+    navegador.get(pessoa)
+    time.sleep(2)
+    navegador.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/div').click()
 
+    # Seleciona o popup e desce com o scroll
+    time.sleep(5)
+    fBody = navegador.find_element_by_xpath("//div[@class='isgrP']")
+    scroll = 0
+    while scroll < 1:
+        navegador.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', fBody)
+        scroll += 1
+
+    # Clica nos botões de seguir
+    for item in range(numero):
+        followButton = navegador.find_element_by_class_name("sqdOP.L3NKy.y3zKF")
+        followButton.click()
+        navegador.find_element_by_xpath('/html/body/div[6]/div/div/div/div[1]/div/div[3]/div/button').click()
+        time.sleep(3)
+        navegador.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/div').click()
+        time.sleep(2)
+
+# Tela do tKinter
 janela = tk.Tk()
+janela.title("Coloque seus dados abaixo para funcionamento do robo")
 
-janela.title("Informações para o funcionamento do robo")
+label_user = tk.Label(janela, text="Usuario do Instagram")
+label_user.grid(row=1, column=0, padx=10, pady=10, sticky='nswe', columnspan=1)
+user = tk.Entry(janela)
+user.grid(row=1, column=1, padx=10, pady=10, sticky='nswe', columnspan=1)
 
-label_login = tk.Label(janela, text="Usuario")
-label_login.grid(row=1, column=0, padx=10, pady=10, sticky='nswe', columnspan=1)
+label_password = tk.Label(janela, text="Senha do Instagram")
+label_password.grid(row=2, column=0, padx=10, pady=10, sticky='nswe', columnspan=1)
+password = tk.Entry(janela)
+password.grid(row=2, column=1, padx=10, pady=10, sticky='nswe', columnspan=1)
 
-entry_login = tk.Entry(janela)
-entry_login.grid(row=1, column=1, padx=10, pady=10, sticky='nswe', columnspan=1)
+label_link = tk.Label(janela, text="Link")
+label_link.grid(row=3, column=0, padx=10, pady=10, sticky='nswe', columnspan=1)
+link = tk.Entry(janela)
+link.grid(row=3, column=1, padx=10, pady=10, sticky='nswe', columnspan=1)
 
-combobox_meio = ttk.Combobox(janela, values=lista_tipos)
-combobox_meio.grid(row=2, column=1, padx=10, pady=10, sticky='nswe', columnspan=4)
+label_num = tk.Label(janela, text="Quantas pessoas seguir?")
+label_num.grid(row=4, column=0, padx=10, pady=10, sticky='nswe', columnspan=1)
+num = tk.Entry(janela)
+num.grid(row=4, column=1, padx=10, pady=10, sticky='nswe', columnspan=1)
 
-botao = tk.Button(janela, text="Exibir dados", command=codigo)
-botao.grid(row=3, column=1, padx=10, pady=10, sticky='nswe', columnspan=4)
-
-texto_login = tk.Label(janela, text="")
-texto_login.grid(row=4, column=1, padx=10, pady=10, sticky='nswe', columnspan=4)
-
-botao2 = tk.Button(janela, text="Abrir outra janela", command=abrir_janela)
-botao2.grid(row=5, column=1, padx=10, pady=10, sticky='nswe', columnspan=4)
-
+botao = tk.Button(janela, text="Buscar seguidores", command=seguidores)
+botao.grid(row=5, column=1, padx=10, pady=10, sticky='nswe', columnspan=4)
 janela.mainloop()
-
